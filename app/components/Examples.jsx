3cfbe82a3682e870f148e438b2847a9a6b40f3ed
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2016-12-01 15:00:08 +0545

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return(
-    <h2>Examples Component</h2>
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to Examples Page!</p>
+    </div>
   );
 }
 
